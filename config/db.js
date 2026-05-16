b1876316dad048200a7e157c359cc1ad7cbc33dd
Manas <manas.sharma2084@gmail.com>
2026-05-16 09:51:46 +0530

    fix: add SSL config to Postgres connection in db.js to fix 500 error on Vercel

--- config/db.js
+++ config/db.js
@@ -11,7 +11,12 @@ const sequelize = isRealDatabaseUrl
         pool: { max: 10, min: 0, acquire: 30000, idle: 10000 },
         dialectOptions: process.env.DATABASE_URL.startsWith('mysql') ? {
             connectTimeout: 60000
-        } : {}
+        } : {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     })
     : new Sequelize({
         dialect: 'sqlite',
